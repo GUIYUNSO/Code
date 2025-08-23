e273fff97bf0385ff7115f9017f526628fb3a238
GUIYUNSO <1816850682@qq.com>
2025-08-23 21:20:56 +0800

    second

---
+++ .vscode/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void butler(void);
+int main(void)
+{
+    printf("I WILL.\n");
+    butler();
+    printf("yes\n");
+    
+    return 0;
+}
+void butler(void)
+{
+    printf("you");
+}
