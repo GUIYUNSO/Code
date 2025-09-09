19188b447d3b484c1f3ca3c45a8346069f269bc9
GUIYUNSO <1816850682@qq.com>
2025-09-09 22:47:18 +0800

    cancel

--- test/compare2.c
+++ test/compare2.c
@@ -2,7 +2,8 @@
 
 int main() {
     int a, b, c;
-    while (scanf("%d %d %d", &a, &b, &c) ) {
+    while (scanf("%d %d %d", &a, &b, &c) ) 
+    {
         int max = a;
         if (b > max) max = b;
         if (c > max) max = c;
