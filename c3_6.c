2be84d8ca1444c96caef41b707f0b82791791ae6
GUIYUNSO <1816850682@qq.com>
2025-09-25 16:25:47 +0800

    rechange

---
+++ c3_6.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float iron=7.86,gold=19.3;
+    float wight_iron,wight_gold,pi=3.1415926,r=5;
+    wight_gold=4.0/3*pi*r*r*r*gold;
+    wight_iron=4.0/3*pi*r*r*r*iron;
+    printf("wight_iron=%f wight_gold=%f\n",wight_iron,wight_gold);
+    return 0;
+}
