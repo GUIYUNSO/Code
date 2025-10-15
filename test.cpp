510e6220f295662f5f25e109fda2c04204430e1d
GUIYUNSO <1816850682@qq.com>
2025-10-15 17:14:42 +0800

    new

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+int a = 10;
+
+// 输出：10
+cout << a << endl;
+
+// 可以串联输出
+// 输出：Hello, World!
+cout << "Hello" << ", " << "World!" << endl;
+
+string s = "abc";
+// 输出：abc 10
+cout << s << " " << a << endl;
+}
